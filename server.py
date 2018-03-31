9d9ae556a88031bf731aab53b942b2a5876ab1cf
honeyyenoh <honeyyenoh@mail.ru>
2018-03-31 14:32:20 +0300

    refactoring: edited server

--- server.py
+++ server.py
@@ -1,51 +1,28 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import request
 import json
-
-app = Flask(__name__)
-connection_string = 'mysql://root:12345678@167.99.192.185:3306/tulasafewalk'
-app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
-db = SQLAlchemy(app)
-
-class TrafficAccident(db.Model):
-	__tablename__ = 'TrafficAccidents'
-	id = db.Column('id', db.Integer, primary_key = True)
-	date = db.Column(db.Integer)
-	latitude = db.Column(db.Float)
-	longitude = db.Column(db.Float)
-	district = db.Column(db.String(30))
-	accident_type =  db.Column(db.String(80))
-
-	def __init__(self, date, latitude, longitude, district, accident_type):
-		self.date = date
-		self.latitude = latitude
-		self.longitude = longitude
-		self.district = district
-		self.accident_type = accident_type
-
-	def as_json(self):
-		return dict(
-			input_id=self.id, date = self.date,
-			latitude = self.latitude, 
-			longitude = self.longitude,
-			district = self.district,
-			accident_type = self.accident_type)
-
+from model import TrafficAccident, app, db
 
 @app.route("/test/", methods=['GET'])
-def get_params():
+def main():
 	begin_date = ''
 	end_date = ''
 	if(request.method == 'GET'):
 		begin_date = request.args.get('begin')
 		end_date = request.args.get('end')
 
-	accidents = TrafficAccident.query.filter(
-		TrafficAccident.date >= begin_date,
-		TrafficAccident.date <= end_date,
+	return get_json_accidents(begin_date, end_date)
+
+def select_data(begin_date, end_date):
+	return TrafficAccident.query.filter(
+			TrafficAccident.date >= begin_date,
+			TrafficAccident.date <= end_date,
 		).all()
+
+def get_json_accidents(begin_date, end_date):
+	accidents = select_data(begin_date, end_date)
 	results = [json.dumps(accident.as_json(), ensure_ascii=False) for accident in accidents]
 	return json.dumps(results, ensure_ascii=False)
+
 
 if __name__ == '__main__':
 	db.create_all()
